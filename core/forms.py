dc693817edd184608d37e2c31465899b46ec13eb
IrakliTabatadze <irakli.tabatadze.80@gmail.com>
2025-02-11 22:11:52 +0400

    Lecture 34

---
+++ core/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Event
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        # fields = ('title', 'description')
+        fields = '__all__'
+
+        # widgets = {
+        #     'title': forms.TextInput(attrs={'class': 'form-control'}),
+        # }
---
+++ core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+LOCATION_CHOICES = (
+    ('shekvetili_arena', 'Shekvetili Arena'),
+    ('meskhis_stationi', 'Meskhis Stationi'),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
+    location = models.CharField(max_length=100, null=False, blank=False, choices=LOCATION_CHOICES)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    max_attendees = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'event'
