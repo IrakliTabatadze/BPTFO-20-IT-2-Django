a7446c36010f4adf70836bc1c90da2235d0b8d6f
IrakliTabatadze <irakli.tabatadze.80@gmail.com>
2025-02-14 21:57:37 +0400

    Lecture 36

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Event
 from .forms import EventForm
 
@@ -10,12 +10,16 @@ def event_list(request):
 
 
 def add_event(request):
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('event_list')
+    if request.user.is_authenticated and request.user.has_perm('core.add_event'):
+        if request.method == 'POST':
+            form = EventForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('event_list')
+        else:
+            form = EventForm()
+            return render(request, 'events/add_event.html', {'form': form})
+
     else:
-        form = EventForm()
-        return render(request, 'events/add_event.html', {'form': form})
+        return HttpResponse("You Do not have permission to do this")
 
